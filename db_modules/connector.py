0258d1c20be9c8a87839c8f492d1cf83a477a84a
Aledangelo <59020122+Aledangelo@users.noreply.github.com>
2022-05-19 17:25:00 +0200

    Update connector.py

--- db_modules/connector.py
+++ db_modules/connector.py
@@ -1,10 +1,11 @@
 import db_modules.mySql as sql
 
-__host = '192.168.1.55'
-__user = 'alessandro'
-__password = 'loportoIO99'
-__port = 3306
-__database = 'networkSecurity'
+## You have to configure your db
+__host = ''
+__user = ''
+__password = ''
+__port = 
+__database = ''
 
 dbSql = sql.MySql(__host, __user, __password, __database, port=__port)
 dbSql.connect()
